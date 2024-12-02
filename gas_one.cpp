a8fcb36d331915dfc8a9ea8ec7c56afcd8893b01
fistandatilus <nikitka236@mail.ru>
2024-12-03 00:39:39 +0300

    Unfinished one step

--- gas_one.cpp
+++ gas_one.cpp
@@ -30,5 +30,30 @@ int progonka(int n, double *a, double *b, double *c, const double *f) {
 
 
 void solve(const P_gas &p_gas, const P_she &p_she, double *v, double *h, double *buf) {
+    double *a, *b, *ch, *cv, *f;
+    double tau = p_she.tau;
+    double h_x = p_she.h_x;
+    double mu = p_gas.mu;
+    int M = p_she.M_x;
+    //один шаг
+    //заполнение для V
+    a = buf;
+    b = buf + M;
+    cv = buf + 2*M;
+    ch = buf + 3*M;
+    f = buf + 4*M;
+
+    double mu_loc = p_gas.mu;
+    double frac = tau*mu_loc/h_x/h_x;
+
+    a[0] = 0; 
+    b[0] = tau/(6*h_x)*v[1] - frac;
+    cv[0] = 1 + frac;
+    f[0] = tau*(p(h[1])/(2*h_x*h[0]) - (mu_loc - mu/h[0])*(v[1] - 2*v[0])/h_x/h_x + func(0))
+    b[M] = tau/(6*h_x)*v[1] - frac;
+    a[M] = 0; 
+    cv[M] = 1 + frac;
+    f[M] = tau*(p(h[1])/(2*h_x*h[0]) - (mu_loc - mu/h[0])*(v[1] - 2*v[0])/h_x/h_x + func(0))
     
+
 }
