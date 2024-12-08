497e0222924d1778654bcfefb14cd3d059601992
fistandatilus <nikitka236@mail.ru>
2024-12-08 12:48:24 +0300

    finished one step

--- gas_one.cpp
+++ gas_one.cpp
@@ -1,9 +1,11 @@
+#include "string.h"
+
 #include "gas_one.h"
 
 
 int progonka(int n, double *a, double *b, double *c, const double *f) {
     double pa = a[0], pb = b[0];
-        
+
     for (int i = 1; i < n; i++){
         double ca, cb, denominator;
         denominator = c[i-1] + pa*a[i-1];
@@ -46,14 +48,49 @@ void solve(const P_gas &p_gas, const P_she &p_she, double *v, double *h, double 
     double mu_loc = p_gas.mu;
     double frac = tau*mu_loc/h_x/h_x;
 
-    a[0] = 0; 
-    b[0] = tau/(6*h_x)*v[1] - frac;
-    cv[0] = 1 + frac;
-    f[0] = tau*(p(h[1])/(2*h_x*h[0]) - (mu_loc - mu/h[0])*(v[1] - 2*v[0])/h_x/h_x + func(0))
-    b[M] = tau/(6*h_x)*v[1] - frac;
-    a[M] = 0; 
-    cv[M] = 1 + frac;
-    f[M] = tau*(p(h[1])/(2*h_x*h[0]) - (mu_loc - mu/h[0])*(v[1] - 2*v[0])/h_x/h_x + func(0))
-    
+    a[0] = 0;
+    double cososim = tau/(6*h_x)*v[1];
+    b[0] = cososim - frac;
+    a[1] = -cososim - frac;
+    cv[0] = 1 + 2*frac;
+    f[0] = tau*(-p(h[1])/(2*h_x*h[0]) - (mu_loc - mu/h[0])*(v[1] - 2*v[0])/h_x/h_x + func(0));
+    cososim = tau/(6*h_x)*v[M-1];
+    a[M] = -cososim - frac;
+    b[M-1] = cososim - frac;
+    b[M] = 0; 
+    cv[M] = 1 + 2*frac;
+    f[M] = tau*(p(h[M-1])/(2*h_x*h[M]) - (mu_loc - mu/h[M])*(v[M-1] - 2*v[M])/h_x/h_x + func(M));
+    for (int i = 1; i <= M-2; i++) {
+        cv[i] = 1 + 2*frac;
+        cososim = tau/(6*h_x)*(v[i] + v[i+1]);
+        b[i] = cososim - frac;
+        a[i+1] = -cososim - frac;
+        f[i] = v[i] + tau*(-(p(h[i+1]) - p(h[i-1]))/2/h_x/h[i] - (mu_loc - mu/h[i])*(v[i+1] - 2*v[i] + v[i-1])/h_x/h_x + func(i));
+    }
+    f[M-1] = v[M-1] + tau*(-(p(h[M]) - p(h[M-2]))/2/h_x/h[M-1] - (mu_loc - mu/h[M-1])*(v[M] - 2*v[M-1] + v[M-2])/h_x/h_x + func(M-1)); //не заполнится в циклe
+    progonka(M+1, a, b, cv, f);
+
+    ch[0] = 1;
+    b[0] = tau/2/h_x*cv[1];
+    a[0] = 0;
+    a[1] = -tau/4/h_x*(cv[0] + cv[1]);
+    f[0] = h[0] - tau/2/h_x*(h[0]*cv[1] - h[2]*v[2] + 2*h[1]*v[1] + 0.5*h[3]*v[3] - h[2]*v[2] + 0.5*h[1]*v[1] - h[0]*v[2] + 2*h[0]*v[1] + 0.5*h[0]*v[3] - h[0]*v[2] + 0.5*h[0]*v[1]);
+    ch[M] = 1;
+    a[M] = tau/2/h_x*v[M-1];
+    b[M] = 0;
+    b[M-1] = tau/4/h_x*(cv[M-1] + cv[M]);
+    f[M] = h[M] - tau/2/h_x*(h[M]*cv[M-1] - h[M-2]*v[M-2] + 2*h[M-1]*v[M-1] + 0.5*h[M-3]*v[M-3] - h[M-2]*v[M-2] + 0.5*h[M-1]*v[M-1] - h[M]*v[M-2] + 2*h[M]*v[M-1] + 0.5*h[M]*v[M-3] - h[M]*v[M-2] + 0.5*h[M]*v[M-1]);
+    for (int i = 1; i <= M-2; i++) {
+        ch[i] = 1;
+        cososim = tau/4/h_x*(cv[i] + cv[i+1]);
+        b[i] = cososim;
+        a[i+1] = -cososim;
+        f[i] = h[i] - tau/8/h_x/h_x*h[i]*(cv[i+1] - cv[i-1]);
+    }
+    f[M-1] = h[M-1] - tau/8/h_x/h_x*h[M-1]*(cv[M] - cv[M-2]);
+    progonka(M+1, a, b, ch, f);
+
+    memcpy(v, cv, (M+1)*sizeof(double));
+    memcpy(h, ch, (M+1)*sizeof(double));
 
 }
